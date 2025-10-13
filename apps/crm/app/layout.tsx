adc518a3fd4e1cdbc78515f74e33371d39b19852
getinfinitas <getinfinitas@proton.me>
2025-10-13 10:35:34 -0600

    Update layout.tsx

--- apps/crm/app/layout.tsx
+++ apps/crm/app/layout.tsx
@@ -35,13 +35,7 @@ export default function RootLayout({
         <SidebarProvider>
           <AppSidebar />
           <SidebarInset>
-            <header className="flex h-16 shrink-0 items-center gap-2 transition-[width,height] ease-linear group-has-[[data-collapsible=icon]]/sidebar-wrapper:h-12">
-              <div className="flex items-center gap-2 px-4">
-                <SidebarTrigger className="-ml-1" />
-                <Separator orientation="vertical" className="mr-2 h-4" />
-              </div>
-            </header>
-            <main className="flex flex-1 flex-col gap-4 p-4 pt-0">
+            <main className="flex flex-1 flex-col gap-4">
               {children}
             </main>
           </SidebarInset>
@@ -52,3 +46,4 @@ export default function RootLayout({
     </html>
   );
 }
+
