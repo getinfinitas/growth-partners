886299834121c7ee9cb62bc18cedd4bddce58b41
getinfinitas <getinfinitas@proton.me>
2025-10-12 22:52:26 -0600

    Update layout.tsx

--- apps/crm/app/layout.tsx
+++ apps/crm/app/layout.tsx
@@ -1,7 +1,7 @@
 import { Inter } from 'next/font/google';
 import { Analytics } from '@vercel/analytics/react';
 import { SpeedInsights } from '@vercel/speed-insights/next';
-import { AppSidebar } from '@/components/dashboard/app-sidebar';
+import { AppSidebar } from '@/components/app-sidebar';
 import {
   SidebarInset,
   SidebarProvider,
