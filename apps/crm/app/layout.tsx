e9d5cd46bf1e89e81b066c518fe4bce85b707ea7
getinfinitas <getinfinitas@proton.me>
2025-10-13 10:59:16 -0600

    Update layout.tsx

--- apps/crm/app/layout.tsx
+++ apps/crm/app/layout.tsx
@@ -2,7 +2,7 @@ import { GeistSans } from 'geist/font/sans'
 import { GeistMono } from 'geist/font/mono'
 import { Analytics } from '@vercel/analytics/react';
 import { SpeedInsights } from '@vercel/speed-insights/next';
-import { ClientAppSidebar } from '@/components/client-app-sidebar';
+import { AppSidebar } from '@/components/app-sidebar';
 import {
   SidebarInset,
   SidebarProvider,
