df107fe12cc9f6ea3248679ce1bd42de36502fbe
getinfinitas <getinfinitas@proton.me>
2025-10-13 21:08:35 -0600

    Update page.tsx

--- apps/crm/app/page.tsx
+++ apps/crm/app/page.tsx
@@ -30,12 +30,12 @@ export default function Page() {
               <BreadcrumbList>
                 <BreadcrumbItem className="hidden md:block">
                   <BreadcrumbLink href="#">
-                    Building Your Application
+                    Infinitas
                   </BreadcrumbLink>
                 </BreadcrumbItem>
                 <BreadcrumbSeparator className="hidden md:block" />
                 <BreadcrumbItem>
-                  <BreadcrumbPage>Data Fetching</BreadcrumbPage>
+                  <BreadcrumbPage>Activation</BreadcrumbPage>
                 </BreadcrumbItem>
               </BreadcrumbList>
             </Breadcrumb>
