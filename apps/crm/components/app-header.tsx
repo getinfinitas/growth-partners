c63ed38d0264558a0af227ffed712ef0fe8d051d
getinfinitas <getinfinitas@proton.me>
2025-10-15 16:17:38 -0600

    Update app-header.tsx

--- apps/crm/components/app-header.tsx
+++ apps/crm/components/app-header.tsx
@@ -24,7 +24,7 @@ interface AppHeaderProps {
 
 export function AppHeader({ title = "Welcome to Infinitas", breadcrumbItems }: AppHeaderProps) {
   return (
-    <header className="flex h-16 shrink-0 items-center gap-2 transition-[width,height] ease-linear group-has-data-[collapsible=icon]/sidebar-wrapper:h-12">
+    <header className="flex h-16 shrink-0 items-center gap-2">
       <div className="flex items-center gap-2 px-4 flex-1">
         <SidebarTrigger className="-ml-1" />
         <Separator
